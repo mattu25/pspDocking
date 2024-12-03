b0b671dd5f1f381f8f80949d95bf6940a37796ac
Matthew Unger <empropartners@gmail.com>
2024-12-03 11:58:24 -0800

    Update moveZips.py

--- workingCode/moveZips.py
+++ workingCode/moveZips.py
@@ -1,14 +1,14 @@
 import os
 import subprocess as sp
 
-directory = "/home/maunger/gninaScoring/pdbqtZips/trancheOutFiles"
+directory = ""
 
 for tranche in os.listdir(directory):
     filesDirectory = os.path.join(directory, tranche)
     for file in os.listdir(filesDirectory):
         if ".zip" in str(file):
             os.chdir(filesDirectory)
-            move = f"mv {tranche}.zip /home/maunger/gninaScoring/pdbqtZips"
+            move = f"mv {tranche}.zip [target directory]"
             sp.run(move, shell=True)
             
             
